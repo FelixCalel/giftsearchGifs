c481a868e7101c5a9fa9fc4d3b6dc918bd788246
FelixCalel <felixcalel806@gmail.com>
2022-04-22 20:56:54 -0600

    Estructura de proyecto

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>Hola
+      <div class="container">
+        <div class="card">
+          <figure>
+            <img src="foto.jpg"></img>
+          </figure>
+          <div class="contenido">
+            <h3>Kratos</h3>
+            <p>El Fantasma de Esparta</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Card
